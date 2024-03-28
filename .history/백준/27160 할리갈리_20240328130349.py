b53cda30589ec32e28d23754c47f3b2e72c93bdc
Guntae Kim <dkfkskgks@naver.com>
2024-03-28 18:01:38 +0900

    백준 문제

---
+++ .history/백준/27160 할리갈리_20240328130349.py
@@ -0,0 +1,33 @@
+# 첫 번째 줄에 펼쳐진 카드의 개수 
+# N이 주어집니다.
+
+# 두 번째 줄부터 
+# N개의 줄에 걸쳐 한 줄에 하나씩 펼쳐진 카드의 정보가 주어집니다.
+
+# 카드의 정보는 공백으로 구분된, 과일의 종류를 나타내는 문자열 
+# S와 과일의 개수를 나타내는 양의 정수 
+# X로 이루어져 있습니다.
+
+# S는 STRAWBERRY, BANANA, LIME, PLUM 중 하나입니다.
+def check(fruit):
+    match fruit:
+        case 'STRAWBERRY':
+            return 0
+        case 'BANANA':
+            return 1
+        case 'LIME':
+            return 2
+        case 'PLUM':
+            return 3
+
+N = int(input())
+f = ['STRAWBERRY', 'BANANA', 'LIME', 'PLUM']
+c = []
+for i in range(N):
+    fruit, count  = input().split()
+    c[check[fruit]] += count
+if 5 in c:
+    print('YES')
+else:
+    print('NO')
+    
