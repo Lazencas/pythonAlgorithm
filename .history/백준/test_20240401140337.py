a7ab73d23e5be836281d2fec5f2a0ccfb7eb070e
Guntae Kim <dkfkskgks@naver.com>
2024-04-01 19:57:20 +0900

    백준 2문제

---
+++ .history/백준/test_20240401140337.py
@@ -0,0 +1,5 @@
+a=[]
+for i in range(2):
+    for j in range(2):
+        a.append([j])    
+print(a)
--- .history/백준/test_20240401140412.py
+++ .history/백준/test_20240401140412.py
@@ -2,4 +2,4 @@ a=[[0]*2 for i in range(2)]
 for i in range(2):
     for j in range(2):
         a[i][j]=j
-print(a.index(1))
+print(a)
