25d5803df39c4bcbd883574974739d9a423655c0
Guntae Kim <dkfkskgks@naver.com>
2024-06-22 20:23:11 +0900

    정리

--- 알고리즘/InsertionSort.py
+++
@@ -1,6 +0,0 @@
-for index in range(데이터길이-1):
-    for index2 in range(index+1,0,-1):
-        if data[index2] < data[index]:
-            swap(data[index2],data[index2-1])
-        else:
-            break
