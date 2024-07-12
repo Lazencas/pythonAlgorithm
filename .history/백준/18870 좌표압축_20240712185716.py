6b2538ea203d5feccb30b5e0c08e21e9c27dd243
Guntae Kim <dkfkskgks@naver.com>
2024-07-12 18:57:26 +0900

    좌표 정리

--- .history/백준/18870 좌표압축_20240712185716.py
+++ .history/백준/18870 좌표압축_20240712185716.py
@@ -8,12 +8,3 @@
 그래서 중복을 제거하고 정렬한 새로운 리스트를 만들고
 새로만든 리스트의 인덱스 번호를
 '''
-import sys
-input = sys.stdin.readline
-n = int(input())
-x = list(map(int,input().split()))
-x2 = sorted(list(set(x)))
-dic = {x2[i] : i for i in range(len(x2))}
-print(dic)
-for i in x:
-    print(dic[i],end=' ')
--- 백준/18870 좌표압축.py
+++ 백준/18870 좌표압축.py
@@ -8,13 +8,3 @@
 그래서 중복을 제거하고 정렬한 새로운 리스트를 만들고
 새로만든 리스트의 인덱스 번호를
 '''
-import sys
-input = sys.stdin.readline
-n = int(input())
-x = list(map(int,input().split()))
-x2 = sorted(list(set(x)))
-#x2의 값을 키 값으로 하고, 밸류 값으로는 인덱스를 넣어준다
-dic = {x2[i] : i for i in range(len(x2))}
-for i in x:
-    #출력할 때 해당 값으로 딕셔너리 조회
-    print(dic[i],end=' ')
